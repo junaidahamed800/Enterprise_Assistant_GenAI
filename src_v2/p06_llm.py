5081e9f7ca1d58dfeaca34733bd54eb0fff08770
Junaid Ahamed Shaik <Junaid.Shaik@prolifics.com>
2026-06-22 18:24:09 +0530

    fix: secrets management for deployment

--- src_v2/p06_llm.py
+++ src_v2/p06_llm.py
@@ -1,24 +1,17 @@
 import os
-from dotenv import load_dotenv
-
+import streamlit as st
 import google.generativeai as genai
 
+def get_api_key():
+    try:
+        return st.secrets["GOOGLE_API_KEY"]  # Streamlit Cloud
+    except:
+        return os.getenv("GOOGLE_API_KEY")   # Local .env
 
-load_dotenv()
+genai.configure(api_key=get_api_key())
 
-genai.configure(
-    api_key=os.getenv("GOOGLE_API_KEY")
-)
-
-model = genai.GenerativeModel(
-    "gemini-2.5-flash"
-)
-
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def generate_answer(prompt):
-
-    response = model.generate_content(
-        prompt
-    )
-
+    response = model.generate_content(prompt)
     return response.text
